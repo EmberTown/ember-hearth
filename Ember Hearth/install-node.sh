8173b6eff67b388b0616835d4acb0b2a36ca2b2f
thomassnielsen <me@thomassnielsen.com>
2015-03-29 16:20:03 +0200

    Remove npm install from node script

--- Ember Hearth/install-node.sh
+++ Ember Hearth/install-node.sh
@@ -6,4 +6,3 @@ cd ~/node-latest-install
 curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
 ./configure --prefix=~/local
 make install # ok, fine, this step probably takes more than 30 seconds...
-curl https://www.npmjs.org/install.sh | sh
