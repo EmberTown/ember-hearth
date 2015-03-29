5081cde3dbad11576790b9fa2f1ac454bb85dc4c
thomassnielsen <me@thomassnielsen.com>
2015-03-29 17:32:43 +0200

    Add terminal helper class

---
+++ Ember Hearth/Terminal.swift
@@ -0,0 +1,61 @@
+//
+//  Terminal.swift
+//  Ember Hearth
+//
+//  Created by Thomas Sunde Nielsen on 29.03.15.
+//  Copyright (c) 2015 Thomas Sunde Nielsen. All rights reserved.
+//
+
+import Cocoa
+
+class Terminal {
+    private var task: NSTask?
+    private var output: NSPipe?
+    
+    init() { }
+    
+    private func taskForCommand (command: String) -> NSTask {
+        var task = NSTask()
+        task.launchPath = "/bin/bash"
+        task.arguments = ["-l", "-c", command]
+        return task
+    }
+    
+    func runTerminalCommandSync (command: String) -> String? {
+        var task = taskForCommand(command)
+        
+        var findOut = NSPipe()
+        task.standardOutput = findOut
+        
+        task.launch()
+        task.waitUntilExit()
+        
+        let outData = findOut.fileHandleForReading.readDataToEndOfFile()
+        let result = NSString(data: outData, encoding: NSASCIIStringEncoding)
+        return result
+    }
+    
+    func runTerminalCommandAsync (command: String, completion: (result: String?) -> ()) {
+        self.task = taskForCommand(command)
+        self.output = NSPipe()
+        self.task?.standardOutput = self.output!
+        
+        self.task?.terminationHandler = { (task: NSTask!) in
+            
+            if task.terminationStatus != 0 {
+                completion(result: nil)
+            } else {
+                let outData = self.output?.fileHandleForReading.readDataToEndOfFile()
+                var result: NSString?
+                if outData != nil {
+                    result = NSString(data: outData!, encoding: NSASCIIStringEncoding)
+                }
+                completion(result: result)
+            }
+            
+            self.task = nil
+            self.output = nil
+        }
+        self.task?.launch()
+    }
+}
