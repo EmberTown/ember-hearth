2cf208cd5a7a08b9a6674f4cb63a50398dafa508
thomassnielsen <me@thomassnielsen.com>
2015-03-29 12:19:39 +0200

    Initial Commit

---
+++ Ember Hearth/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Ember Hearth
+//
+//  Created by Thomas Sunde Nielsen on 29.03.15.
+//  Copyright (c) 2015 Thomas Sunde Nielsen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Ember Hearth/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Ember Hearth
+//
+//  Created by Thomas Sunde Nielsen on 29.03.15.
+//  Copyright (c) 2015 Thomas Sunde Nielsen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
