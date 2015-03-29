f22f60c7e804b11597605acb8821dce1e2af7f30
thomassnielsen <me@thomassnielsen.com>
2015-03-29 16:21:12 +0200

    Add node install script and node class with path, version and installed check

---
+++ Ember Hearth/NPM.swift
@@ -0,0 +1,61 @@
+//
+//  NPM.swift
+//  Ember Hearth
+//
+//  Created by Thomas Sunde Nielsen on 29.03.15.
+//  Copyright (c) 2015 Thomas Sunde Nielsen. All rights reserved.
+//
+
+import Cocoa
+
+class NPM {
+    class func isInstalled () -> Bool {
+        return version() != nil
+    }
+    
+    class func path() -> String? {
+        var findNodeTask = NSTask()
+        findNodeTask.launchPath = "/bin/bash"
+        findNodeTask.arguments = ["-l", "-c", "which npm"]
+        
+        var findOut = NSPipe()
+        findNodeTask.standardOutput = findOut
+        
+        findNodeTask.launch()
+        findNodeTask.waitUntilExit()
+        
+        let outData = findOut.fileHandleForReading.readDataToEndOfFile()
+        let path = NSString(data: outData, encoding: NSASCIIStringEncoding)
+        
+        println("Path: \(path)")
+        
+        return path
+    }
+    
+    class func version () -> String? {
+        var checkNodeVersion = NSTask()
+        checkNodeVersion.launchPath = "/bin/bash"
+        checkNodeVersion.arguments = ["-l", "-c", "npm -v"]
+        
+        var checkOut = NSPipe()
+        checkNodeVersion.standardOutput = checkOut
+        
+        checkNodeVersion.launch()
+        checkNodeVersion.waitUntilExit()
+        
+        let checkData = checkOut.fileHandleForReading.readDataToEndOfFile()
+        
+        let version = NSString(data: checkData, encoding: NSUTF8StringEncoding)
+        
+        println("Version: \(version)")
+        
+        if version?.length > 0 {
+            return version
+        }
+        return nil
+    }
+    
+    class func install ((success: Bool) -> ()) {
+        
+    }
+}
