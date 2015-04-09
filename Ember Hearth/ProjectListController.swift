f6ac264f10b26b0531e211b9447b57467bb8e15e
thomassnielsen <me@thomassnielsen.com>
2015-04-09 20:15:26 +0200

    Configure project list in interface builder

--- Ember Hearth/ProjectListController.swift
+++ Ember Hearth/ProjectListController.swift
@@ -73,36 +73,13 @@ class ProjectListController: NSViewController, NSTableViewDataSource, NSTableVie
         return count
     }
     
-    var rowHeight: CGFloat = 50.0
-    var textSize: CGFloat = 20.0
-    
     func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
-        var label = NSTextField()
-        label.bezeled = false
-        label.editable = false
-        label.selectable = false
-        label.backgroundColor = NSColor.clearColor()
+        var view: NSTableCellView = tableView.makeViewWithIdentifier("projectCell", owner: tableView) as! NSTableCellView
         var project: Project = projects![row]
-        label.stringValue = project.name!
-        label.font = NSFont.systemFontOfSize(textSize)
-        let labelHeight = textSize + textSize * 0.2
-        label.frame = NSMakeRect(0, 0, self.tableView.frame.size.width, labelHeight)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        
-        var wrapper = NSTableRowView()
-        wrapper.backgroundColor = NSColor.clearColor()
-        wrapper.addSubview(label)
-        
-        let margin = (rowHeight - labelHeight) / 2
-        wrapper.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("|-15-[label]-15-|", options: nil, metrics: nil, views: ["label":label]))
-        wrapper.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-margin-[label]-margin-|", options: nil, metrics: ["margin":margin], views: ["label":label]))
-        return wrapper
+        view.textField!.stringValue = project.name!
+        return view
     }
 
-    func tableView(tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
-        return rowHeight
-    }
-    
     func tableViewSelectionDidChange(notification: NSNotification) {
         if projects == nil || projects?.count == 0 {
             return
