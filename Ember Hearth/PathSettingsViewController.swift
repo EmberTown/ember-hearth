0a4a99abc0c7e688895c20824907aa0262a5c17b
thomassnielsen <me@thomassnielsen.com>
2015-04-10 22:45:23 +0200

    Show detected default path for tools as placeholder in settings

---
+++ Ember Hearth/PathSettingsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  PathSettingsViewController.swift
+//  Ember Hearth
+//
+//  Created by Thomas Sunde Nielsen on 10.04.15.
+//  Copyright (c) 2015 Thomas Sunde Nielsen. All rights reserved.
+//
+
+import Cocoa
+
+class PathSettingsViewController: NSViewController {
+
+    @IBOutlet var nodePathTextField: NSTextField!
+    @IBOutlet var npmPathTextField: NSTextField!
+    @IBOutlet var bowerPathTextField: NSTextField!
+    @IBOutlet var phantomJSPathTextField: NSTextField!
+    @IBOutlet var emberCLIPathTextField: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nodePathTextField.placeholderString = Node.path()
+        npmPathTextField.placeholderString = NPM.path()
+        bowerPathTextField.placeholderString = Bower.path()
+        phantomJSPathTextField.placeholderString = PhantomJS.path()
+        emberCLIPathTextField.placeholderString = EmberCLI.path()
+    }
+    
+}
