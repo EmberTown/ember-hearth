97989f24b9f6c11fc6a99655c505b68fa3821e78
thomassnielsen <me@thomassnielsen.com>
2015-04-11 17:40:41 +0200

    Use $HOME instead of ~ to fix install bug for node

--- Ember Hearth/install-node.sh
+++ Ember Hearth/install-node.sh
@@ -1,9 +1,10 @@
 # via https://gist.github.com/isaacs/579814
 echo 'export PATH=$HOME/local/bin:$PATH' >> ~/.bashrc
 . ~/.bashrc
+cd ~
 mkdir ~/local
 mkdir ~/node-latest-install
 cd ~/node-latest-install
 curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
-./configure --prefix=~/local
+./configure --prefix=$HOME/local
 make install
