11ad0578adcf6029a91091ab7a29bb1abe619f09
thomassnielsen <me@thomassnielsen.com>
2015-04-12 16:23:43 +0200

    Copy bashrc to bash_profile if no bash_profile exists. Fixes #40

--- Ember Hearth/install-node.sh
+++ Ember Hearth/install-node.sh
@@ -1,7 +1,12 @@
 # via https://gist.github.com/isaacs/579814
+if [ ! -f ~/.bash_profile ]; then
+  cp ~/.bashrc ~/.bash_profile
+fi
+
 if ! grep "PATH=\$HOME/local/bin:\$PATH" ~/.bashrc ; then
  echo 'export PATH=$HOME/local/bin:$PATH' >> ~/.bashrc
 fi
+
 if ! grep "PATH=\$HOME/local/bin:\$PATH" ~/.bash_profile ; then
     echo 'export PATH=$HOME/local/bin:$PATH' >> ~/.bash_profile
 fi
