363d13d28d812f8172a3a16ce22f191b6b91d2e8
thomassnielsen <me@thomassnielsen.com>
2015-04-13 18:58:15 +0200

    Define the basic actions of the projectController

---
+++ Ember Hearth/ProjectController.swift
@@ -0,0 +1,27 @@
+//
+//  ProjectController.swift
+//  Ember Hearth
+//
+//  Created by Thomas Sunde Nielsen on 13.04.15.
+//  Copyright (c) 2015 Thomas Sunde Nielsen. All rights reserved.
+//
+
+import Cocoa
+
+class ProjectController {
+    @IBAction func createProject(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func openProject(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func runServer(sender: AnyObject) {
+    
+    }
+
+    @IBAction func stopServer(sender: AnyObject) {
+        
+    }
+}
