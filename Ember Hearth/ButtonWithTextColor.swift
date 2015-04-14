a3cf952e43721731cd50b1822c00d1b0b5cbabf7
thomassnielsen <me@thomassnielsen.com>
2015-04-14 20:41:14 +0200

    Fix up some button color issues after switching to "textured" buttons

---
+++ Ember Hearth/ButtonWithTextColor.swift
@@ -0,0 +1,28 @@
+//
+//  buttonWithTextColor.swift
+//  Ember Hearth
+//
+//  Created by Thomas Sunde Nielsen on 14.04.15.
+//  Copyright (c) 2015 Thomas Sunde Nielsen. All rights reserved.
+//
+
+import Cocoa
+
+@IBDesignable
+class ButtonWithTextColor: NSButton {
+    @IBInspectable var textColor: NSColor = NSColor.blackColor()
+    override var title: String {
+        set {
+            var attributedString = NSMutableAttributedString(string: title)
+            attributedString.addAttribute(NSForegroundColorAttributeName, value: NSColor.blackColor(), range: NSMakeRange(0, attributedString.length))
+            var style = NSMutableParagraphStyle()
+            style.alignment = .CenterTextAlignment
+            attributedString.addAttribute(NSParagraphStyleAttributeName, value: style, range: NSMakeRange(0, attributedString.length))
+            attributedString.addAttribute(NSBaselineOffsetAttributeName, value: -1, range: NSMakeRange(0, attributedString.length))
+            self.attributedTitle = attributedString
+        }
+        get {
+            return self.attributedTitle.string
+        }
+    }
+}
