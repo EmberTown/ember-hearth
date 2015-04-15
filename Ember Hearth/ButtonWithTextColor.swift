a0d816a9c5eaa0b20319c5cededb35c7b365c4ea
thomassnielsen <me@thomassnielsen.com>
2015-04-15 10:33:10 +0200

    Respect set color in ButtonWithTextColor

--- Ember Hearth/ButtonWithTextColor.swift
+++ Ember Hearth/ButtonWithTextColor.swift
@@ -10,11 +10,15 @@ import Cocoa
 
 @IBDesignable
 class ButtonWithTextColor: NSButton {
-    @IBInspectable var textColor: NSColor = NSColor.blackColor()
+    @IBInspectable var textColor: NSColor = NSColor.blackColor() {
+        didSet {
+            self.title = self.attributedTitle.string
+        }
+    }
     override var title: String {
         set {
             var attributedString = NSMutableAttributedString(string: title)
-            attributedString.addAttribute(NSForegroundColorAttributeName, value: NSColor.blackColor(), range: NSMakeRange(0, attributedString.length))
+            attributedString.addAttribute(NSForegroundColorAttributeName, value: textColor, range: NSMakeRange(0, attributedString.length))
             var style = NSMutableParagraphStyle()
             style.alignment = .CenterTextAlignment
             attributedString.addAttribute(NSParagraphStyleAttributeName, value: style, range: NSMakeRange(0, attributedString.length))
