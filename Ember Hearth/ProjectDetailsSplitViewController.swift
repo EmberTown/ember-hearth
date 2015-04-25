6bceeb0d86b39571e95dfae6199a10ebd37c1be1
thomassnielsen <me@thomassnielsen.com>
2015-04-25 19:28:33 +0200

    Disable draggable hover state for project details split view Fixes #78

---
+++ Ember Hearth/ProjectDetailsSplitViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProjectDetailsSplitViewController.swift
+//  Ember Hearth
+//
+//  Created by Thomas Sunde Nielsen on 25.04.15.
+//  Copyright (c) 2015 Thomas Sunde Nielsen. All rights reserved.
+//
+
+import Cocoa
+
+class ProjectDetailsSplitViewController: NSSplitViewController {
+    override func splitView(splitView: NSSplitView, effectiveRect proposedEffectiveRect: NSRect, forDrawnRect drawnRect: NSRect, ofDividerAtIndex dividerIndex: Int) -> NSRect {
+        return CGRectNull
+    }
+}
